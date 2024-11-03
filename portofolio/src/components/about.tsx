ccba733a4e5185ff44b93ceb1a32f914736ddcdd
kemalmusth <kemalmusthafa80@gmail.com>
2024-11-03 14:11:43 +0700

    Updated 01 November 2024

---
+++ portofolio/src/components/about.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image"
+
+function About() {
+    return (
+      <div className="relative bg-[#0b1e5e] w-screen min-h-screen flex flex-col items-center gap-10 p-5">
+        {/* About Title */}
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-yellow-200 mb-5 mt-20">ABOUT</h1>
+        </div>
+  
+        {/* Logo Section */}
+        <div className="flex flex-col md:flex-row items-center justify-between gap-20">
+          {/* Logo Unjani */}
+          <div className="w-[120px] h-[120px] sm:w-[150px] sm:h-[155px] overflow-hidden shadow-lg rounded-lg">
+            <img
+              src="/img/Logo_unjani.png"
+              alt="Logo Unjani"
+              className="w-full h-full object-cover"
+            />
+          </div>
+  
+          {/* Purwadhika Logo */}
+          <div className=" w-80 h-20 md:w-[300px] md:h-[100px] overflow-hidden rounded-xl shadow-lg">
+            <img
+              src="/img/purwadhika.png"
+              alt="Purwadhika"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+  
+        {/* SVG Wave at the Bottom */}
+        <div className="absolute bottom-0 left-0 w-full overflow-hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            preserveAspectRatio="none"
+            className="w-full h-[150px] md:h-[200px]"
+          >
+            <path
+              fill="white"
+              fillOpacity="1"
+              d="M0,96L80,122.7C160,149,320,203,480,186.7C640,171,800,85,960,53.3C1120,21,1280,43,1360,53.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            />
+          </svg>
+        </div>
+      </div>
+    );
+  }
+  
+  export default About;
